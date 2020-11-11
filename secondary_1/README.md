dbdfe33d03d82ee3a5b79d3a25eeaf6bbe7405e5
Orest Ohorodnyk <o.ohorodnyk@gmail.com>
2020-11-11 18:57:55 +0200

    removed redandent files

--- secondary_1/README.md
+++
@@ -1,5 +0,0 @@
-##Build container:
-``docker build -t master_app .``
-
-##Run container
-``docker run -d -p 8000:8000 master_app``
--- secondary_2/app/message_model.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from dataclasses import dataclass
-
-
-@dataclass
-class Message:
-    message: str
-    created_at: datetime = datetime.utcnow()
